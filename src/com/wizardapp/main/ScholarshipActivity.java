628a6b14af0580040979fb7ea93cf6852b722925
anuradha_singh <anuradha.ga1@gmail.com>
2016-01-10 18:38:37 +0530

    fhgdfgd

--- src/com/wizardapp/main/ScholarshipActivity.java
+++ src/com/wizardapp/main/ScholarshipActivity.java
@@ -2,14 +2,34 @@ package com.wizardapp.main;
 
 import com.example.wizardapp.R;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ScholarshipActivity extends MyBaseActivity{
+	ListView listview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.scholarship_list);
+		
+		showCustomActionBar();
+		listview=(ListView)findViewById(R.id.scholarship_listview);
+				
+		
 	}
+	private void showCustomActionBar() {
+		// TODO Auto-generated method stub
+		final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater()
+				.inflate(R.layout.custom_actionbar, null);
 
+		// Set up your ActionBar
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowHomeEnabled(false);
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayShowCustomEnabled(true);
+		actionBar.setCustomView(actionBarLayout);
+	}
 }
