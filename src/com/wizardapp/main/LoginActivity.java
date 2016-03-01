159698d1d36aba4b73d1e19a1c2adff3a855b8b6
anuradha_singh <anuradha.ga1@gmail.com>
2016-03-01 23:46:52 +0530

    nbhghj

--- src/com/wizardapp/main/LoginActivity.java
+++ src/com/wizardapp/main/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.wizardapp.main;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.http.ParseException;
+
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.wizardapp.R;
 import com.wizardapp.fragments.LoginFragment;
+import com.wizardapp.fragments.RegisterFragment;
+import com.wizardapp.utils.DateUtil;
 import com.wizardapp.utils.SharedPreferencesHelper;
 
 public class LoginActivity extends MyBaseActivity{
 
 	SharedPreferences  mPrefs  ;
 	Editor prefsEditor ;
+	 public static final int DATE_OF_BIRTH = 0;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,6 +54,52 @@ public class LoginActivity extends MyBaseActivity{
 		    	// TODO Auto-generated method stub
 		    	super.onStart();
 		    }
-		    
-		    
+		    @Override
+			protected Dialog onCreateDialog(int id) {
+				Dialog r = null;
+				switch (id) {
+				case LoginActivity.DATE_OF_BIRTH:
+					try {
+						Date startDate;
+						try {
+							startDate = DateUtil.parse(RegisterFragment.dateOfBith.getText().toString());
+							OnDateSetListener listener = new DateSetListener(RegisterFragment.dateOfBith);
+							int year = DateUtil.getYear(startDate);
+							int month = DateUtil.month(startDate);
+							int dayOfMonth = DateUtil.dayOfMonth(startDate);
+							r = new DatePickerDialog(this, listener, year, month, dayOfMonth);
+						} catch (java.text.ParseException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}
+					catch (ParseException e) {
+						e.printStackTrace();
+					}
+					break;
+				
+				}
+				return r;
+			}
+			
+		    public static class DateSetListener implements OnDateSetListener {
+				private EditText button;
+
+				public DateSetListener(EditText button) {
+					super();
+					this.button = button;
+				}
+
+				@Override
+				public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+					Calendar c = Calendar.getInstance();
+					c.set(year, monthOfYear, dayOfMonth);
+					updateButtonDisplay(button, c.getTime());
+				}
+			}
+			private static void updateButtonDisplay(EditText button, Date dateToSet) {
+				button.setText(new SimpleDateFormat("dd-MM-yyyy").format(dateToSet));
+			}
+
 		    }
