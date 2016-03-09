5e25588e3206dbd6657cfb211c89b64aa7903c7b
nitin- <nitin.jha583@gmail.com>
2016-03-09 11:26:39 +0530

    sdkdksdjks

--- src/com/wizardapp/utils/Util.java
+++ src/com/wizardapp/utils/Util.java
@@ -1,26 +1,4 @@
 package com.wizardapp.utils;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Random;
 
-public class Util {
-
-	static Random rnd = new Random();
-	static Collection<Integer> alreadyChosen = new HashSet<Integer>();
-
-    public static int getNextUniqueRandom(int maxSize){
-        if (alreadyChosen.size()==maxSize){ //hardcoded 5 figure numbers, consider making a variable
-             throw new RuntimeException("All values used");
-        }
-
-        boolean unique=false;
-        int value=0;
-        while(unique==false){
-            value=rnd.nextInt(maxSize)+10000;
-            unique=!alreadyChosen.contains(value);
-        }
-        alreadyChosen.add(value);
-        return value;
-    }
-}
+public class Util {}
